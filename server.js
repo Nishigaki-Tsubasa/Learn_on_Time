e3236470b3758e85b09798a5b37442792a6c5b03
moja <kd1342160@st.kobedenshi.ac.jp>
2024-10-14 16:49:11 +0900

    コメントつけた

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const { google } = require('googleapis');
 const path = require('path');
 const app = express();
-const port = 80;
+const port = 80; //80番
 
 
 // MongoDB接続情報
